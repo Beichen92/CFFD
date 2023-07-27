f0cb16a4eeec78918fe54da1c3ddcd68c6480bcb
Beichen92 <129265343+Beichen92@users.noreply.github.com>
2023-07-27 11:14:05 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = '*.920324.xyz'
+      url.hostname = 'allsouls.alwaysdata.net'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
